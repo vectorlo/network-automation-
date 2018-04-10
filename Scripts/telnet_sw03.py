dbb7fa20ae73e32e8a617cdfef6492220a7c03b1
vectorlo <31265295+vectorlo@users.noreply.github.com>
2018-04-10 15:59:15 +0800

    Update telnet_sw03.py

--- Scripts/telnet_sw03.py
+++ Scripts/telnet_sw03.py
@@ -4,28 +4,27 @@
 import getpass
 import telnetlib
 
-
 user = "cisco"
 password = "cisco"
 
 for n in range(1,11):
     host = "10.0.0.0 %s" % n
-	tn = telnetlib.Telnet(HOST)
+    tn = telnetlib.Telnet(HOST)
 
-	tn.read_until(b"Username: ")
-	tn.write(user.encode('ascii') + b"\n")
-	if password:
+    tn.read_until(b"Username: ")
+    tn.write(user.encode('ascii') + b"\n")
+    if password:
     	tn.read_until(b"Password: ")
     	tn.write(password.encode('ascii') + b"\n")
 
-	tn.write(b"enable\n")
-	tn.write(b"cisco\n")
-	for n in range(2,11):
+    tn.write(b"enable\n")
+    tn.write(b"cisco\n")
+    for n in range(2,11):
     	vlan = "vlan %s" % n
     	desc = "name Python_VLAN %s" % n
     	tn.write(vlan.encode('ascii') + b"\n")
 		tn.write(desc.encode('ascii') + b"\n")
 	tn.write(b"end\n")
 	tn.write(b"exit\n")
-	
+
 	print(tn.read_all().decode('ascii'))
